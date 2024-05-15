8effb9f6474d1452b14e0304efc8cbff1faa0bef
AndhikaRCP <andhikarizky2603@gmail.com>
2024-05-15 23:21:41 +0700

    fix percentage

--- result.js
+++ result.js
@@ -4,20 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function generateTable() {
     const tableBody = document.getElementById('generatedTable').getElementsByTagName('tbody')[0];
-    let diseasesWithPercentage = localStorage.getItem("hasilPenyakit");
+    let penyakitsWithPercentage = localStorage.getItem("hasilPenyakit");
 
     // Check if data exists in localStorage
-    if (diseasesWithPercentage) {
+    if (penyakitsWithPercentage) {
         // Parse the data into JSON format
-        diseasesWithPercentage = JSON.parse(diseasesWithPercentage);
+        penyakitsWithPercentage = JSON.parse(penyakitsWithPercentage);
         
         // Iterate over the data and add rows to the table
-        diseasesWithPercentage.forEach(({ disease, percentage }, index) => {
+        penyakitsWithPercentage.forEach(({ penyakit, percentage }, index) => {
             const row = tableBody.insertRow();
             const cell1 = row.insertCell(0);
             const cell2 = row.insertCell(1);
             
-            cell1.textContent = `${disease.name}`;
+            cell1.textContent = `${penyakit.name}`;
             cell2.textContent = `${percentage.toFixed(2)}%`;
         });
     } else {
--- script.js
+++ script.js
@@ -1,183 +1,163 @@
-const diseases = [
+const penyakits = [
     { 
         name: "CONTRACT ULCERS",
-        symptoms: [3, 16]
+        gejalas: [3, 16]
     },
     { 
         name: "ABAES PARAFARINGEAL",
-        symptoms: [3, 19]
+        gejalas: [3, 19]
     },
     { 
         name: "ABAES PERITONAILER",
-        symptoms: [1, 2, 7, 14, 16, 22]
+        gejalas: [1, 2, 7, 14, 16, 22]
     },
     { 
         name: "BAROTITIS MEDIA",
-        symptoms: [2, 6]
+        gejalas: [2, 6]
     },
     { 
         name: "DEVIASI SEPTUM",
-        symptoms: [1, 5, 6, 15, 25, 29]
+        gejalas: [1, 5, 6, 15, 25, 29]
     },
     { 
         name: "FARINGITIS",
-        symptoms: [1, 3, 7, 13, 14]
+        gejalas: [1, 3, 7, 13, 14]
     },
     { 
         name: "KANKER LARING",
-        symptoms: [3, 4, 7, 13, 16, 23, 24]
+        gejalas: [3, 4, 7, 13, 16, 23, 24]
     },
     { 
         name: "KANKER LEHER DAN KEPALA",
-        symptoms: [3, 12, 15, 21, 30, 31]
+        gejalas: [3, 12, 15, 21, 30, 31]
     },
     { 
         name: "KANKER LEHER METASTATIK",
-        symptoms: [12]
+        gejalas: [12]
     },
     { 
         name: "KANKER NASOFARING",
-        symptoms: [5, 15]
+        gejalas: [5, 15]
     },
     { 
         name: "KANKER TONSIL",
-        symptoms: [7, 12]
+        gejalas: [7, 12]
     },
     { 
         name: "LARINGITIS",
-        symptoms: [1, 3, 14, 19, 37]
+        gejalas: [1, 3, 14, 19, 37]
     },
     { 
         name: "NEURONITIS VESTIBULARIS",
-        symptoms: [10, 17]
+        gejalas: [10, 17]
     },
     { 
         name: "OSTEOSKLEROSIS",
-        symptoms: [20, 35]
+        gejalas: [20, 35]
     },
     { 
         name: "OTITIS MEDIA AKUT",
-        symptoms: [1, 6, 10, 32]
+        gejalas: [1, 6, 10, 32]
     },
     { 
         name: "MENIERE",
-        symptoms: [6, 10, 34, 36]
+        gejalas: [6, 10, 34, 36]
     },
     { 
         name: "TONSILITIS",
-        symptoms: [1, 2, 3, 4, 7, 10]
+        gejalas: [1, 2, 3, 4, 7, 10]
     },
     { 
         name: "TUMOR SYARAF PENDENGARAN",
-        symptoms: [2, 20, 38]
+        gejalas: [2, 20, 38]
     },
     { 
         name: "VERTIGO POSTULAR",
-        symptoms: [17]
+        gejalas: [17]
     },
     { 
         name: "SINUSITIS MAKSILARIS",
-        symptoms: [1, 2, 4, 5, 8, 9, 11, 28, 33]
+        gejalas: [1, 2, 4, 5, 8, 9, 11, 28, 33]
     },
     { 
         name: "SINUSITIS FRONTALIS",
-        symptoms: [1, 2, 4, 5, 8, 9, 11, 18]
+        gejalas: [1, 2, 4, 5, 8, 9, 11, 18]
     },
     { 
         name: "SINUSITIS ETMOIDALIS",
-        symptoms: [1, 2, 4, 5, 8, 9, 11, 18, 26, 27]
+        gejalas: [1, 2, 4, 5, 8, 9, 11, 18, 26, 27]
     },
     { 
         name: "SINUSITIS SFENOIDALIS",
-        symptoms: [1, 2, 4, 5, 6, 8, 9, 11, 12]
+        gejalas: [1, 2, 4, 5, 6, 8, 9, 11, 12]
     },
     { 
         name: "PERUT",
-        symptoms: [1, 2, 3, 4]
+        gejalas: [1, 2, 3, 4]
     },
     
 ];
 
-function findDiseases(symptoms) {
-    const matchedDiseases = [];
+function findpenyakits(gejalas) {
+    const matchedpenyakits = [];
     
-    diseases.forEach(disease => {
-        const hasMatchedSymptom = disease.symptoms.some(symptom => symptoms.includes(symptom));
-        if (hasMatchedSymptom) {
-            matchedDiseases.push(disease);
+    penyakits.forEach(penyakit => {
+        const hasMatchedgejala = penyakit.gejalas.some(gejala => gejalas.includes(gejala));
+        if (hasMatchedgejala) {
+            matchedpenyakits.push(penyakit);
         }
     });
     
-    return matchedDiseases;
+    return matchedpenyakits;
 }
 
-function generatePopupMessage(possibleDiseases, checkedSymptoms) {
-    // Membuat array untuk menyimpan penyakit beserta persentase cocok
-    const diseasesWithPercentage = [];
 
-    possibleDiseases.forEach(disease => {
-        // Menghitung jumlah gejala yang cocok
-        const matchedSymptoms = disease.symptoms.filter(symptom => checkedSymptoms.includes(symptom));
-        const percentage = (matchedSymptoms.length / disease.symptoms.length) * 100;
+function generatePopupMessage(possiblepenyakits, checkedgejalas) {
+    const penyakitsWithPercentage = [];
 
-        // Menambahkan penyakit beserta persentase ke array
-        diseasesWithPercentage.push({ disease, percentage });
+    possiblepenyakits.forEach(penyakit => {
+        const matchedgejalas = penyakit.gejalas.filter(gejala => checkedgejalas.includes(gejala));
+        const matchCount = matchedgejalas.length;
+        
+        // Persentase berdasarkan jumlah gejala yang cocok dengan total gejala penyakit
+        const percentagePenyakit = (matchCount / penyakit.gejalas.length) * 100;
+        
+        // Persentase berdasarkan jumlah gejala yang cocok dengan total gejala yang diperiksa pengguna
+        const percentageUser = (matchCount / checkedgejalas.length) * 100;
+        
+        // Menggunakan rata-rata kedua persentase
+        const averagePercentage = (percentagePenyakit + percentageUser) / 2;
+
+        penyakitsWithPercentage.push({ penyakit, percentage: averagePercentage, matchCount });
     });
 
-    // Mengurutkan penyakit berdasarkan persentase cocok dari yang tertinggi ke terendah
-    diseasesWithPercentage.sort((a, b) => b.percentage - a.percentage);
+    // Mengurutkan penyakit berdasarkan persentase cocok dan jumlah gejala yang cocok dari yang tertinggi ke terendah
+    penyakitsWithPercentage.sort((a, b) => {
+        if (b.percentage === a.percentage) {
+            return b.matchCount - a.matchCount; // Prioritaskan penyakit dengan jumlah gejala yang cocok terbanyak
+        }
+        return b.percentage - a.percentage;
+    });
 
-    // Membuat pesan berdasarkan urutan penyakit
-    let message = "Anda mungkin menderita penyakit berikut:\n\n";
-    // diseasesWithPercentage.forEach(({ disease, percentage }) => {
-    //     message += `${disease.name}: ${percentage.toFixed(2)}%\n`;
-    // });
-    return diseasesWithPercentage;
-    
+    return penyakitsWithPercentage;
 }
 
-
-
-
-function getCheckedSymptoms() {
-    const checkedSymptoms = [];
+function getCheckedgejalas() {
+    const checkedgejalas = [];
     const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
     checkboxes.forEach(checkbox => {
-        checkedSymptoms.push(parseInt(checkbox.id));
+        checkedgejalas.push(parseInt(checkbox.id));
     });
-    return checkedSymptoms;
+    return checkedgejalas;
 }
 
-function displayPossibleDiseases(possibleDiseases) {
-    console.log("No.\tNAMA PENYAKIT\t\t\tGEJALA PENYAKIT");
-    possibleDiseases.forEach((disease, index) => {
-        const symptomsString = disease.symptoms.join(" ");
-        console.log(`${index + 1}\t${disease.name}\t\t\t${symptomsString}`);
-    });
-}
-
-
-
-document.getElementById('getCheckedSymptomsBtn').addEventListener('click', () => {
+document.getElementById('getCheckedgejalasBtn').addEventListener('click', () => {
     event.preventDefault();
-    const checkedSymptoms = getCheckedSymptoms();
-    const possibleDiseases = findDiseases(checkedSymptoms);
-    displayPossibleDiseases(possibleDiseases);
-    
-    const hasilPenyakit = generatePopupMessage(possibleDiseases, checkedSymptoms);
+    const checkedgejalas = getCheckedgejalas();
+    const possiblepenyakits = findpenyakits(checkedgejalas);
+    const hasilPenyakit = generatePopupMessage(possiblepenyakits, checkedgejalas);
     let dataKontans = JSON.stringify(hasilPenyakit);
     localStorage.setItem("hasilPenyakit", dataKontans);
-    alert(hasilPenyakit);
+
     window.location.href= "result.html";
 });
-
-
-
-// function generatePopupMessage(possibleDiseases) {
-//     let message = "Anda mungkin menderita penyakit berikut:\n\n";
-//     possibleDiseases.forEach((disease, index) => {
-//         const symptomsString = disease.symptoms.join(", ");
-//         message += `${disease.name}`;
-//     });
-//     return message;
-// }
